ef89cccb43e406dd190a386c89dab5a4342bb525
Doniyor <doniyork334@gmail.com>
2025-12-24 00:33:46 +0500

    Initial commit with basic configurations

---
+++ src/main/java/org/example/teachhive/TeachhiveApplication.java
@@ -0,0 +1,13 @@
+package org.example.teachhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachhiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachhiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/teachhive/TeachhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teachhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachhiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
